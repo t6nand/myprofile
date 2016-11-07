99f49347650c81c951ca182de1b4e451bfe08971
Tapan Sharma <tapan@roposo.com>
2016-11-07 16:41:18 +0530

    used path.join for managing routes

--- app/profile.js
+++ app/profile.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.use(express.static(__dirname + '/../views'));
+app.use(express.static(path.join(__dirname + '/../views')));
 
 app.get('/', function(req,res) {
     res.redirect('/profile');
