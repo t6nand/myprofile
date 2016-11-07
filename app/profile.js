c5a0b1f2a48b51ca7846fb8d13cbe152c00f8564
Tapan Sharma <tapan@roposo.com>
2016-11-07 11:45:28 +0530

    Basic Frontend and Backend added.
    1. Added backend controller based on express.
    2. Added pages in view.
    3. Added partials like css, fonts, js, etc.
    4. added package.json

---
+++ app/profile.js
@@ -0,0 +1,34 @@
+/*
+    This server file handles all routing and control which component to show in view for a particular
+    action made in web. 
+*/
+
+// Native & 3rd party libraries.
+var bodyParser = require('body-parser');
+var express = require('express');
+var ejs = require('ejs');
+var path = require('path');
+var app = express();
+
+process.setMaxListeners(0);
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static(__dirname + '/../views'));
+
+app.get('/', function(req,res) {
+    res.redirect('/profile');
+})
+// index page 
+app.get('/profile', function(req, res) {
+    res.render('index');
+});
+
+app.listen(3000);
+
+console.log('MyProfile listening on 4000.');
